ad1e00d5c5cd17f40d9cc674f87cf113d2fdaa9a
BishalAryal345 <sirajaryal69@gmail.com>
2021-07-07 12:23:07 +0545

    try

--- models/register_model.js
+++ models/register_model.js
@@ -9,14 +9,14 @@ const Register = mongoose.model('Register',{
         type: String,
         required: true
     },
-    // PhoneNo:{
-    //     type : String,
-    //     required: true
-    // },
-    // Username:{
-    //     type : String,
-    //     required: true
-    // },
+    PhoneNo:{
+        type : String,
+        required: true
+    },
+    Username:{
+        type : String,
+        required: true
+    },
     Password:{
         type : String,
         required:true
